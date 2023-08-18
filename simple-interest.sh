55757e747855b1ef21ea9e0f54e2d643199329a6
ADELAANI <88137905+ADELAANI@users.noreply.github.com>
2023-08-18 09:39:56 +0100

    Create simple-interest.sh
    a simple interest class using javascript

---
+++ simple-interest.sh
@@ -0,0 +1,11 @@
+// Define the variables to store the principal, rate of interest, and time
+var p = 1000; // Principal amount
+var r = 0.05; // Rate of interest
+var t = 2;    // Time in years
+
+// Calculate the interest using the formula
+// Simple Interest = (principal * rate * time) / 100
+var interest = (p * r * t) / 100;
+
+// Display the result
+console.log("Simple Interest: " + interest);
